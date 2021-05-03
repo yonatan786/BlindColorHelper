a9f6081dea7fb94b2d619fd5dba426c9c56ca46c
yonatan786 <yonatan786@gmail.com>
2021-05-03 13:51:48 +0300

    Revert "second commit"
    This reverts commit 57086289ae2655de0009b311533760ccec455fb4.

--- colorBlindHelper/app/src/main/java/com/example/colorblindhelper/ui/Tabs/settings.kt
+++ colorBlindHelper/app/src/main/java/com/example/colorblindhelper/ui/Tabs/settings.kt
@@ -11,7 +11,7 @@ import com.example.colorblindhelper.R
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-//yonatan
+
 /**
  * A simple [Fragment] subclass.
  * Use the [settings.newInstance] factory method to
